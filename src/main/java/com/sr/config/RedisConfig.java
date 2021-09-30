fa13608245e8fbd02ede26351161933b635ad7bb
lctaba <1169858807@qq.com>
2021-09-30 19:33:55 +0800

    feat[cyh]:redis配置

---
+++ src/main/java/com/sr/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.sr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Author cyh
+ * @Date 2021/9/28 10:32
+ */
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(redisSerializer);
+        redisTemplate.setValueSerializer(redisSerializer);
+        redisTemplate.setHashKeySerializer(redisSerializer);
+        redisTemplate.setHashValueSerializer(redisSerializer);
+        return redisTemplate;
+    }
+}
