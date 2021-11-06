50305b9d3458c2b32ef720b4d22ef26de102b9b1
WTBantoe <928871624@qq.com>
2021-11-06 19:08:14 +0800

    fix[wt]:修改文件路径的写法

--- src/main/java/com/sr/service/impl/SRServiceImpl.java
+++ src/main/java/com/sr/service/impl/SRServiceImpl.java
@@ -2,6 +2,7 @@ package com.sr.service.impl;
 
 import com.sr.common.PythonExecutionUtils;
 import com.sr.service.SRService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -9,8 +10,30 @@ import java.io.File;
 @Service
 public class SRServiceImpl implements SRService
 {
-    static File image_python_file = new File("src/test/testfiles/ESRGAN/imageSR.py");
-    static File video_python_file = new File("videoSR.py");
+    static File image_python_file;
+    static File video_python_file;
+
+    public File getImage_python_file()
+    {
+        return image_python_file;
+    }
+
+    @Value("${model.path.image}")
+    public void setImage_python_file(String image_python_file)
+    {
+        SRServiceImpl.image_python_file = new File(image_python_file);
+    }
+
+    public File getVideo_python_file()
+    {
+        return video_python_file;
+    }
+
+    @Value("${model.path.video}")
+    public void setVideo_python_file(String video_python_file)
+    {
+        SRServiceImpl.video_python_file = new File(video_python_file);
+    }
 
     @Override
     public String[] imageSuperResolution()
