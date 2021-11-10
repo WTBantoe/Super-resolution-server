3abcef16c0372e136d71e67e6421c6f513f78d5f
lctaba <1169858807@qq.com>
2021-11-10 17:19:07 +0800

    Revert "style[wt]:格式化代码"
    This reverts commit a4d69d19

--- src/main/java/com/sr/controller/HistoryController.java
+++ src/main/java/com/sr/controller/HistoryController.java
@@ -2,6 +2,9 @@ package com.sr.controller;
 
 import com.sr.common.HttpUtil;
 import com.sr.common.ReturnCodeBuilder;
+import com.sr.entity.History;
+import com.sr.entity.User;
+import com.sr.entity.builder.UserBuilder;
 import com.sr.service.HistoryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -24,80 +27,129 @@ import java.util.Map;
 @RequestMapping("/history")
 @Api(tags = {"历史记录管理"})
 @RestController
-public class HistoryController
-{
+public class HistoryController {
     @Autowired
     HistoryService historyService;
 
     @Autowired
     HttpUtil httpUtil;
 
-    @ApiOperation(value = "修改时间倒序获取用户历史记录", notes = "修改时间倒序获取用户历史记录")
-    @RequestMapping(value = "/modify/desc", method = RequestMethod.GET)
-    @Transactional(rollbackFor = Exception.class)
+    @ApiOperation(
+            value = "修改时间倒序获取用户历史记录",
+            notes = "修改时间倒序获取用户历史记录"
+    )
+    @RequestMapping(
+            value = "/modify/desc",
+            method = RequestMethod.GET
+    )
+    @Transactional(
+            rollbackFor = Exception.class
+    )
 
-    public Map<String, Object> getUserHistoryListByModifyTimeDESC(@RequestParam(value = "page", required = true) Long page, @RequestParam(value = "pageSize", required = true) Integer pageSize, @RequestParam(value = "tag", required = false) String tag, HttpServletRequest httpServletRequest)
-    {
+    public Map<String, Object> getUserHistoryListByModifyTimeDESC (@RequestParam(value = "page", required = true) Long page,
+                                    @RequestParam(value = "pageSize", required = true) Integer pageSize,
+                                    @RequestParam(value = "tag", required = false) String tag,
+                                    HttpServletRequest httpServletRequest){
         Long uid = httpUtil.getUidByToken(httpUtil.getToken(httpServletRequest));
-        List<Map<String, Object>> mapList = historyService.getUserHistoryListByModifyTimeDESC(uid, page, pageSize, tag);
+        List<Map<String, Object>> mapList = historyService.getUserHistoryListByModifyTimeDESC(uid,page,pageSize,tag);
 
-        return ReturnCodeBuilder.successBuilder().addDataValue(mapList).buildMap();
+        return ReturnCodeBuilder.successBuilder()
+                .addDataValue(mapList)
+                .buildMap();
     }
 
-    @ApiOperation(value = "获取用户的历史数据标签", notes = "获取用户的历史数据标签")
-    @RequestMapping(value = "/tags", method = RequestMethod.GET)
-    @Transactional(rollbackFor = Exception.class)
+    @ApiOperation(
+            value = "获取用户的历史数据标签",
+            notes = "获取用户的历史数据标签"
+    )
+    @RequestMapping(
+            value = "/tags",
+            method = RequestMethod.GET
+    )
+    @Transactional(
+            rollbackFor = Exception.class
+    )
 
-    public Map<String, Object> getUserTags(HttpServletRequest httpServletRequest)
-    {
+    public Map<String, Object> getUserTags (HttpServletRequest httpServletRequest){
         Long uid = httpUtil.getUidByToken(httpUtil.getToken(httpServletRequest));
 
         List<String> list = historyService.getUserTags(uid);
 
-        return ReturnCodeBuilder.successBuilder().addDataValue(list).buildMap();
+        return ReturnCodeBuilder.successBuilder()
+                .addDataValue(list)
+                .buildMap();
     }
 
-    @ApiOperation(value = "获取用户的历史数据数量", notes = "获取用户的历史数据数量")
-    @RequestMapping(value = "count", method = RequestMethod.GET)
-    @Transactional(rollbackFor = Exception.class)
+    @ApiOperation(
+            value = "获取用户的历史数据数量",
+            notes = "获取用户的历史数据数量"
+    )
+    @RequestMapping(
+            value = "count",
+            method = RequestMethod.GET
+    )
+    @Transactional(
+            rollbackFor = Exception.class
+    )
 
-    public Map<String, Object> getUserHistoryCount(HttpServletRequest httpServletRequest)
-    {
+    public Map<String, Object> getUserHistoryCount (HttpServletRequest httpServletRequest){
         Long uid = httpUtil.getUidByToken(httpUtil.getToken(httpServletRequest));
         long count = historyService.getCountByUid(uid);
         Map<String, Object> map = new HashMap<>();
         map.put("count", count);
 
-        return ReturnCodeBuilder.successBuilder().addDataValue(map).buildMap();
+        return ReturnCodeBuilder.successBuilder()
+                .addDataValue(map)
+                .buildMap();
     }
 
 
-    @ApiOperation(value = "删除一条历史数据", notes = "删除一条历史数据")
-    @RequestMapping(value = "", method = RequestMethod.DELETE)
-    @Transactional(rollbackFor = Exception.class)
+    @ApiOperation(
+            value = "删除一条历史数据",
+            notes = "删除一条历史数据"
+    )
+    @RequestMapping(
+            value = "",
+            method = RequestMethod.DELETE
+    )
+    @Transactional(
+            rollbackFor = Exception.class
+    )
 
-    public Map<String, Object> deleteById(HttpServletRequest httpServletRequest, @RequestParam(required = true) Long historyId)
-    {
+    public Map<String, Object> deleteById (HttpServletRequest httpServletRequest,
+                                           @RequestParam(required = true)Long historyId){
         Long uid = httpUtil.getUidByToken(httpUtil.getToken(httpServletRequest));
         boolean deleted = historyService.deleteById(historyId, uid);
         Map<String, Object> map = new HashMap<>();
         map.put("deleted", deleted ? "删除成功" : "删除失败");
 
-        return ReturnCodeBuilder.successBuilder().addDataValue(map).buildMap();
+        return ReturnCodeBuilder.successBuilder()
+                .addDataValue(map)
+                .buildMap();
     }
 
-    @ApiOperation(value = "批量删除历史数据", notes = "批量删除历史数据")
-    @RequestMapping(value = "batch", method = RequestMethod.DELETE)
-    @Transactional(rollbackFor = Exception.class)
+    @ApiOperation(
+            value = "批量删除历史数据",
+            notes = "批量删除历史数据"
+    )
+    @RequestMapping(
+            value = "batch",
+            method = RequestMethod.DELETE
+    )
+    @Transactional(
+            rollbackFor = Exception.class
+    )
 
-    public Map<String, Object> deleteByIdList(HttpServletRequest httpServletRequest, @RequestParam(required = true) List<Long> historyIds)
-    {
+    public Map<String, Object> deleteByIdList (HttpServletRequest httpServletRequest,
+                                           @RequestParam(required = true)List<Long> historyIds){
         Long uid = httpUtil.getUidByToken(httpUtil.getToken(httpServletRequest));
         long count = historyService.deleteByIdList(historyIds, uid);
         Map<String, Object> map = new HashMap<>();
         map.put("count", count);
 
-        return ReturnCodeBuilder.successBuilder().addDataValue(map).buildMap();
+        return ReturnCodeBuilder.successBuilder()
+                .addDataValue(map)
+                .buildMap();
     }
 
 }
--- src/main/java/com/sr/service/impl/HistoryServiceImpl.java
+++ src/main/java/com/sr/service/impl/HistoryServiceImpl.java
@@ -13,7 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -23,8 +25,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @Date 2021/11/2 16:49
  */
 @Service
-public class HistoryServiceImpl implements HistoryService
-{
+public class HistoryServiceImpl implements HistoryService {
     @Autowired
     HistoryMapper historyMapper;
 
@@ -32,19 +33,16 @@ public class HistoryServiceImpl implements HistoryService
     HttpUtil httpUtil;
 
     @Override
-    public List<Map<String, Object>> getUserHistoryListByModifyTimeDESC(Long uid, Long page, Integer pageSize, String tag)
-    {
+    public List<Map<String, Object>> getUserHistoryListByModifyTimeDESC(Long uid, Long page, Integer pageSize, String tag) {
         HistoryExample historyExample = new HistoryExample();
         HistoryExample.Criteria criteria = historyExample.createCriteria();
         criteria.andUidEqualTo(uid);
-        if (!StringUtils.isEmpty(tag))
-        {
+        if (!StringUtils.isEmpty(tag)){
             criteria.andTagEqualTo(tag);
         }
 
         historyExample.setOrderByClause("gmt_modify DESC");
-        if (page > 0)
-        {
+        if (page > 0) {
             Long offset = (page - 1L) * pageSize;
             historyExample.setLimit(pageSize);
             historyExample.setOffset(offset);
@@ -55,8 +53,7 @@ public class HistoryServiceImpl implements HistoryService
         return EntityMapConvertor.entityList2MapList(histories);
     }
 
-    public HistoryExample getExampleByUid(Long uid)
-    {
+    public HistoryExample getExampleByUid(Long uid) {
         HistoryExample historyExample = new HistoryExample();
         HistoryExample.Criteria criteria = historyExample.createCriteria();
         criteria.andUidEqualTo(uid);
@@ -64,15 +61,12 @@ public class HistoryServiceImpl implements HistoryService
     }
 
     @Override
-    public Map<String, Object> post(History history)
-    {
-        try
-        {
+    public Map<String, Object> post(History history) {
+        try {
             long id = historyMapper.insertSelective(history);
             history.setId(id);
         }
-        catch (Exception e)
-        {
+        catch (Exception e){
             throw new StatusException(StatusEnum.HISTORY_INSERT_FAIL);
         }
         return EntityMapConvertor.entity2Map(history);
@@ -80,45 +74,35 @@ public class HistoryServiceImpl implements HistoryService
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public List<Map<String, Object>> postList(List<History> histories)
-    {
-        List<Map<String, Object>> historyMapList = new ArrayList<>();
-        for (History history : histories)
-        {
+    public List<Map<String, Object>> postList(List<History> histories) {
+        List<Map<String, Object>> historyMapList = new ArrayList<>(); 
+        for (History history : histories){
             historyMapList.add(post(history));
         }
         return historyMapList;
     }
 
     @Override
-    public List<String> getUserTags(Long uid)
-    {
+    public List<String> getUserTags(Long uid) {
         return historyMapper.getUserTags(uid);
     }
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public boolean deleteById(Long id, Long uid)
-    {
+    public boolean deleteById(Long id, Long uid) {
         History history = historyMapper.selectByPrimaryKey(id);
-        if (history == null)
-        {
+        if (history == null) {
             throw new StatusException(StatusEnum.HISTORY_NOT_EXIST);
         }
-        if (history.getUid() == null)
-        {
+        if (history.getUid() == null) {
             throw new StatusException(StatusEnum.NOT_CORRECT_USER);
         }
-        if (!history.getUid().equals(uid))
-        {
+        if (!history.getUid().equals(uid)) {
             throw new StatusException(StatusEnum.NOT_CORRECT_USER);
         }
-        try
-        {
+        try{
             historyMapper.deleteByPrimaryKey(id);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new StatusException(StatusEnum.HISTORY_DELETE_FAILED);
         }
         return true;
@@ -126,20 +110,17 @@ public class HistoryServiceImpl implements HistoryService
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public long getCountByUid(Long uid)
-    {
+    public long getCountByUid(Long uid) {
         HistoryExample historyExample = getExampleByUid(uid);
         return historyMapper.countByExample(historyExample);
     }
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public long deleteByIdList(List<Long> historyIds, Long uid)
-    {
+    public long deleteByIdList(List<Long> historyIds, Long uid) {
         AtomicInteger count = new AtomicInteger();
-        for (Long historyId : historyIds)
-        {
-            deleteById(historyId, uid);
+        for (Long historyId : historyIds) {
+            deleteById(historyId,uid);
             count.addAndGet(1);
         }
         return count.get();
